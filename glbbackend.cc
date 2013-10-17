f4b11276abcc59797327fd9fc516a595df91e888
Brandon Logan <brandon.logan@rackspace.com>
2013-10-16 22:00:26 -0500

    First commit with random ip and make files.

---
+++ glbbackend.cc
@@ -0,0 +1,97 @@
+/*
+    PowerDNS Versatile Database Driven Nameserver
+    Copyright (C) 2002 - 2008  PowerDNS.COM BV
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2
+    as published by the Free Software Foundation
+    
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include "pdns/utility.hh"
+#include "pdns/dnsbackend.hh"
+#include "pdns/dns.hh"
+#include "pdns/dnsbackend.hh"
+#include "pdns/dnspacket.hh"
+#include "pdns/ahuexception.hh"
+#include "pdns/logger.hh"
+#include <boost/algorithm/string.hpp>
+
+/* FIRST PART */
+class GLBBackend : public DNSBackend
+{
+public:
+  GLBBackend(const string &suffix="") 
+  {
+  }
+
+  bool list(const string &target, int id) {
+    return false; // we don't support AXFR
+  }
+    
+  void lookup(const QType &type, const string &qdomain, DNSPacket *p, int zoneId)
+  {
+      ostringstream os;
+      os<<Utility::random()%256<<"."<<Utility::random()%256<<"."<<Utility::random()%256<<"."<<Utility::random()%256;
+      d_answer=os.str();                                           // our random ip address
+  }
+
+  bool get(DNSResourceRecord &rr)
+  {
+    if(!d_answer.empty()) {
+      rr.qname=d_ourname;                                           // fill in details
+      rr.qtype=QType::A;                                            // A record
+      rr.ttl=5;                                                 // 5 seconds
+      rr.auth = 1;  // it may be random.. but it is auth!
+      rr.content=d_answer;
+
+      d_answer="";                                                  // this was the last answer
+      
+      return true;
+    }
+    return false;                                                   // no more data
+  }
+  
+private:
+  string d_answer;
+  string d_ourname;
+};
+
+/* SECOND PART */
+
+class GLBFactory : public BackendFactory
+{
+public:
+  GLBFactory() : BackendFactory("glb") {}
+  void declareArguments(const string &suffix="")
+  {
+  }
+  DNSBackend *make(const string &suffix="")
+  {
+    return new GLBBackend(suffix);
+  }
+};
+
+/* THIRD PART */
+
+class GLBLoader
+{
+public:
+  GLBLoader()
+  {
+    BackendMakers().report(new GLBFactory);
+    
+    L<<Logger::Info<<" [GLBBackend] This is the glbbackend version "VERSION" ("__DATE__", "__TIME__") reporting" << endl;
+  }  
+};
+
+static GLBLoader glbLoader;
+
